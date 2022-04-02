5e84a84a60b4b82b2bc7d87d9f0af60f25c5d9b8
Quinn0823 <3547347506@qq.com>
2022-04-03 00:20:10 +0800

    💻 v10.0 Pages

---
+++ v10.0/js/click.js
@@ -0,0 +1,5 @@
+jQuery(document).ready(function($) {
+    $("body").click(function(e) {
+        window.location = '/home';
+    });
+});
---
+++ v10.0/js/command.js
@@ -0,0 +1,4 @@
+function command() {
+    var command = document.getElementById('command').value;
+    window.location = 'https://quinn0823.github.io/v10.0/'+command;
+}
